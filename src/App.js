566da7b8e2bb73a1ad0c3ad710693dd8d9e6fa87
mohitparmar2429 <mohitparmar2429@gmail.com>
2025-01-05 01:44:27 +0530

    Footer_adjusted

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import Home from './Pages/Home';
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import './App.css';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-// import 'boxicons/css/boxicons.min.css';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//     <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//       </Routes>
-//       <Footer />
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,22 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>
-          <i className="bx bx-envelope"></i> mohitparmar2429@gmail.com
+        <p>Designed and coded with ❤️ by Mohit <br/>
+          Powered by coffee and late-night coding.
+        </p>
+        <p>Driven by Curiosity, Defined by Results.<br/>
+        <div className="social-media">
+                    <a href="https://github.com/Max-tx"
+                        target="_blank" rel="noopener noreferrer"><i className='bx bxl-github'></i></a>
+                    <a href="https://www.instagram.com/mohitparmar_4g08/" target="_blank" rel="noopener noreferrer"><i
+                            className='bx bxl-instagram'></i></a>
+                    <a href="https://www.linkedin.com/in/mohit-parmar-b34932240" target="_blank" rel="noopener noreferrer"><i
+                            className='bx bxl-linkedin'></i></a>
+                </div>
         </p>
         <p>
-          all <i className="bx bx-copyright"></i> rights reserved by BUNNY
+          <i className="bx bx-envelope"></i> mohitparmar2429@gmail.com  <br/>
+          <i className="bx bx-copyright"></i>2024 Mohit. All Rights Reserved.
         </p>
       </div>
     </footer>
